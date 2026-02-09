f9a8f37a820af5c01af611f757236e39235c57dc
DENG924 <danilo.diaferia@hotmail.it>
2026-02-09 10:11:02 +0800

    Update STappTest1.py

--- STappTest1.py
+++ STappTest1.py
@@ -1,77 +1,45 @@
+#cd C:\Users\admin\Desktop\my_venv\venvTK\Scripts
+#Activate
+#streamlit run "C:\Users\Public\DAN\Danilo2\MasterRE\Modulo6 - Wind\PY_M6_wind\Resource_analysis_ST\welcome.py"
+
 import streamlit as st
-import pandas as pd
-import plotly.graph_objects as go
 
-st.title("Time series data analysis")
-#st.text("")
+st.set_page_config(page_title="Welcome")#, page_icon=":material/waving_hand:")
+st.title("Welcome to Wind Resource Analisys 👋")
+st.sidebar.success("To start go to Upload file page!")
+st.write(
+    """
+    To start please use the **Upload file** page at left bar 👈
 
+    ### What can this tool do?
+    - Calculate the Wind Speed average, maximum, minimum values on a Hourly, Monthly, Annual basis
+    - Calculate the Air Density take in to account the air pression and temperature decrease with the height
+    - Calculate parameters of interest at the specified height, such as wind speed, air density, turbulence index, etc.
+    - Calculate Weibull parameters
+    - Time series data analisys
+    - Wind rose analisys
+    - Weibull analisys
+    - Turbolence analisys
 
-uploaded_file = st.file_uploader("Choose a file",accept_multiple_files=False, type=["txt", "csv"]) #https://docs.streamlit.io/develop/api-reference/widgets/st.file_uploader
-if uploaded_file is not None:
-    df_datos_eolicos = pd.read_csv(uploaded_file, sep='\t', decimal=",", header=0,encoding='ISO-8859-1')
-    st.text("File loaded.")
-
-    # Convertir la columna de fecha a formato datetime para luego hacer grupby y calcular valores para agrupo de anos, mes etc..
-    df_datos_eolicos["Date/Time"]=pd.to_datetime(df_datos_eolicos["Date/Time"])
-
-    #A table to dispaly the dataframe
-    st.text("Table ")
-    st.write(df_datos_eolicos.head(2))
-    #st.write(df_datos_eolicos.loc["2017-01-01"])  #https://www.geeksforgeeks.org/python/python-pandas-dataframe-loc/
-
-    #convertir la columna Date/time en indice Dataframe para facilitar analisi temporal
-    df_datos_eolicos.set_index("Date/Time", inplace=True)
-
-    #Create dropdowns menu____________________________
-    variable = st.selectbox("Select a variable:", df_datos_eolicos.columns.to_list() )
-
-    frecuencia1 = st.selectbox("Select a frequency:", ['H (Hourly)', 'D (Daily)', 'W (Weekly)','M (Monthly)','Y (Yearly)'],index=1) #indiex to pre select a particular item in the list
-    #frecuencia: "H" hora, "D, diaria, W (Semanal), M (mensual), A (anual)
-    
-    frecuencia="D"
-    if frecuencia1=="H (Hourly)": frecuencia="H"
-    if frecuencia1=="D (Daily": frecuencia="D"
-    if frecuencia1=="W (Weekly)": frecuencia="W"
-    if frecuencia1=="M (Monthly)": frecuencia="ME"
-    if frecuencia1=="Y (Yearly)": frecuencia="YE"
-    #st.write(frecuencia)
-
-    #funcion grafico______________________________-
-    #variable="Velocidad a 100m [m/s]"
-    #frecuencia="D"
-    #Agrupar segun la frequencia especificada
-    df_agrupado=df_datos_eolicos[variable].resample(frecuencia).mean().reset_index() #DF di una sola variable
-
-    #crear figura interactiva con Ploty
-    fig=go.Figure()
-    fig.add_trace(go.Scatter(x=df_agrupado["Date/Time"],y=df_agrupado[variable], mode="lines",name=variable))
-
-    #configurar el titulo y etiquetas
-    fig.update_layout(
-        title=f"Time series of {variable} with frequency ({frecuencia})",
-        xaxis_title="Date",
-        yaxis_title=variable,
-        template="plotly_white",
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list([
-                dict(count=7, label="Last week",step="day", stepmode="backward"),
-                dict(count=1, label="Last month",step="month", stepmode="backward"),
-                dict(count=6, label="Lasts 6 months",step="month", stepmode="backward"),
-                dict(count=1, label="Last year",step="year", stepmode="backward"),
-                dict(step="all")
-                ])
-            ),
-            rangeslider=dict(visible=True), #deslizador de fechas
-            type="date"
-        )
-    )
-
-    #mostrar la figura
-    st.plotly_chart(fig, config = {'scrollZoom': False})
-
-# A button that displays text when clicked
-#if st.button("About"):
-#    st.text("Welcome to GeeksForGeeks!")
+    ### The following formulas will be use
+     """
+)
+st.write("")
+st.write("1. Temperature variation with altitude [link](https://en.wikipedia.org/wiki/Lapse_rate)")
+st.latex(r'''T(h)=T(z_0​)-L \cdot (h−z_0​) ''')
+st.write("2. Pressure variation with altitude [link](https://es.wikipedia.org/wiki/F%C3%B3rmula_barom%C3%A9trica)")
+st.latex(r'''P(h)=P0 \cdot \left(1-\frac{L\cdot (h-h0)}{T_0}\right)^{(\frac{-g M}{RL})} ''')
+st.write("3. Air density variation with altitude")
+st.latex(r'''\rho(h)=\frac{P(h)M}{RT(h)}''')
+st.write("4. Power density density variation with altitude")
+st.latex(r'''P=\frac{1}{2} \cdot \rho \cdot V^3''')
 
 
+# Preload Python modules that take a while to compile in a new venv.
+# Otherwise, when users switch to another page, it seems that Streamlit
+# is slow, when in reality this is just an artifact of loading/compiling
+# large modules from zero.
+with st.spinner("Preloading Python modules for other pages..."):
+    import numpy  # noqa: ICN001 F401
+    import pandas  # noqa: ICN001 F401
+    import plotly
